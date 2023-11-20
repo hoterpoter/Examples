d34aa266fa8745bb433ceebac9b0c549c71e485c
Oleg Seifullin <sensei.lis@yandex.com>
2023-11-21 00:30:56 +0200

    добавили логические условия

---
+++ Example005_bool/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "Kira")
+{
+   Console.WriteLine("Хай моя дорогая!!!");
+}
+else
+{
+   Console.Write("Привет, ");
+   Console.Write(username);
+}
