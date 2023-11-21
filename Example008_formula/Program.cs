789b97f23ea3d851abc1a9b339632b1a6361bd4b
Oleg Seifullin <sensei.lis@yandex.com>
2023-11-21 12:48:37 +0200

    немного преобразили ответ

--- Example008_formula/Program.cs
+++ Example008_formula/Program.cs
@@ -2,14 +2,19 @@
 // Результат вывести на экран.
 
 int a = new Random().Next(1, 50);
+Console.Write("Число a: ");
 Console.WriteLine(a);
 int b = new Random().Next(1, 50);
+Console.Write("Число b: ");
 Console.WriteLine(b);
 int c = new Random().Next(1, 50);
+Console.Write("Число c: ");
 Console.WriteLine(c);
 int d = new Random().Next(1, 50);
+Console.Write("Число d: ");
 Console.WriteLine(d);
 
-int formula = ((a * b) / (c + d));
+double formula = (a * b) / (c + d);
 
+Console.Write("Ваш ответ: ");
 Console.WriteLine(formula);
